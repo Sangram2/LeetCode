f9967ce19400f249e37cc8520603fe426201dcf3
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-04-03 12:09:02 +0530

    Time: 1 ms (69.83%), Space: 45.2 MB (56.03%) - LeetHub

--- 563-binary-tree-tilt/563-binary-tree-tilt.java
+++ 563-binary-tree-tilt/563-binary-tree-tilt.java
@@ -37,11 +37,30 @@ class Solution {
         me.tilt=Math.abs(lp.sum-rp.sum)+lp.tilt+rp.tilt;
         return me;
     }
+    
+    
+    static int tilt=0;
+    public int solve2(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int ls=solve2(root.left);
+        int rs=solve2(root.right);
+        
+       
+        int sum=ls+rs+root.val;
+        tilt+=Math.abs(ls-rs);
+        return sum;
+    }
     public int findTilt(TreeNode root) {
         if(root==null || (root.left==null && root.right==null)){
             return 0;
         }
-        pair ans=solve(root);
-        return ans.tilt;
+        // pair ans=solve(root);
+        //return ans.tilt;
+        tilt=0;
+        solve2(root);
+        return tilt;
+        
     }
 }
