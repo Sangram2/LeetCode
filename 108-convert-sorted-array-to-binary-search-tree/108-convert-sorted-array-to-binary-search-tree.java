58304c66581e042d58b705e034a028018f73cef6
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-04-08 19:32:37 +0530

    Time: 1 ms (27.38%), Space: 43.9 MB (35.59%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
@@ -22,7 +22,7 @@ class Solution {
         // if(st==en){   //Although this base case will not required if you have already written the upper base case
         //     return new TreeNode(nums[st]);
         // }
-        int mid=(st+en)/2;
+        int mid=st+((en-st)/2);
         TreeNode node=new TreeNode(nums[mid]);
         node.left=construct(nums,st,mid-1);
         node.right=construct(nums,mid+1,en);
