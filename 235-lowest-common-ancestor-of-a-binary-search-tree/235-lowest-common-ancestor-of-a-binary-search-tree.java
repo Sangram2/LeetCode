6655c8d7df41824db9585551c326141c3126f311
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-04-09 12:22:11 +0530

    Time: 9 ms (27.33%), Space: 50.2 MB (34.17%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,15 +9,33 @@
  */
 
 class Solution {
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     if(root==p || root==q || (p.val<root.val && q.val>root.val) || (p.val>root.val && q.val<root.val)){
+    //         return root;
+    //     }
+    //     else if(p.val<root.val && q.val<root.val){
+    //         return lowestCommonAncestor(root.left,p,q);
+    //     }
+    //     else{
+    //         return lowestCommonAncestor(root.right,p,q);
+    //     }
+    // }
+    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==p || root==q || (p.val<root.val && q.val>root.val) || (p.val>root.val && q.val<root.val)){
-            return root;
+        TreeNode curr = root;
+        while(curr != null){
+            if(p.val < curr.val && q.val < curr.val){
+                curr = curr.left;
+            }
+            else if(p.val > curr.val && q.val > curr.val){
+                curr = curr.right;
+            }
+            else{
+                //divergent
+                return curr;
+            }
         }
-        else if(p.val<root.val && q.val<root.val){
-            return lowestCommonAncestor(root.left,p,q);
-        }
-        else{
-            return lowestCommonAncestor(root.right,p,q);
-        }
+        
+        return null;
     }
 }
