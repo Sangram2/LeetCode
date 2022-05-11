d845ad4e492ab7b10aaf1ebb6fa17c1a9b580f0b
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-05-11 22:18:45 +0530

    Time: 3 ms (64.87%), Space: 43.3 MB (31.51%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -44,8 +44,15 @@ class Solution {
             }
             t++;
         }
-        
         return frashOranges!=0 ? -1:t;
+        // for(int i=0;i<grid.length;i++){
+        //     for(int j=0;j<grid[0].length;j++){
+        //         if(grid[i][j]==1){
+        //             return -1;
+        //         }
+        //     }
+        // }
+        // return t;
 
     }
 }
