135d927860d1bde10c60ea476035257037a5eb23
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-05-29 19:12:57 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (27.95%) - LeetHub

---
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode findNode(TreeNode root,int x){
+        if(root==null){
+            return null;
+        }
+        if(root.val==x){
+            return root;
+        }
+        if(root.left!=null){
+            TreeNode find=findNode(root.left,x);
+            if(find!=null){
+                return find;
+            }
+        }
+        return findNode(root.right,x);
+    } 
+    
+    public int size(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int leftlen=size(root.left);
+        int rightlen=size(root.right);
+        return leftlen+rightlen+1;
+    }
+    
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        TreeNode p1=findNode(root,x);
+        
+        int totalSize=size(root);
+        int sizeOfP1=size(p1);
+        
+        int sizeleft=size(p1.left);
+        int sizeright=size(p1.right);
+        int remaining= totalSize-sizeOfP1;
+        
+        int minrq=(n+1)/2;
+        int p2=Math.max(sizeleft,Math.max(sizeright,remaining));
+        if(p2>=minrq){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
