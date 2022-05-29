31a149af08984c68ef839a09444571d7a7b33fc1
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-05-29 19:48:45 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (18.11%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -14,45 +14,52 @@
  * }
  */
 class Solution {
-    public TreeNode findNode(TreeNode root,int x){
-        if(root==null){
-            return null;
-        }
-        if(root.val==x){
-            return root;
-        }
-        if(root.left!=null){
-            TreeNode find=findNode(root.left,x);
-            if(find!=null){
-                return find;
-            }
-        }
-        return findNode(root.right,x);
-    } 
-    
-    public int size(TreeNode root){
+    // public TreeNode findNode(TreeNode root,int x){
+    //     if(root==null){
+    //         return null;
+    //     }
+    //     if(root.val==x){
+    //         return root;
+    //     }
+    //     if(root.left!=null){
+    //         TreeNode find=findNode(root.left,x);
+    //         if(find!=null){
+    //             return find;
+    //         }
+    //     }
+    //     return findNode(root.right,x);
+    // } 
+    int xKaleft;
+    int xKaright;
+    public int size(TreeNode root,int x){
         if(root==null){
             return 0;
         }
-        int leftlen=size(root.left);
-        int rightlen=size(root.right);
+        int leftlen=size(root.left,x);
+        int rightlen=size(root.right,x);
+        
+        if(root.val==x){
+            xKaleft=leftlen;
+            xKaright=rightlen;
+        }
         return leftlen+rightlen+1;
     }
     
     public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
-        TreeNode p1=findNode(root,x);
+        // TreeNode p1=findNode(root,x);
         
-        int totalSize=size(root);
+        int totalSize=size(root,x);
         
         
-        int sizeleft=size(p1.left);
-        int sizeright=size(p1.right);
+        //int sizeleft=size(p1.left);
+        //int sizeright=size(p1.right);
         
-        int sizeOfP1 =sizeleft+sizeright+1;
+        // int sizeOfP1 =sizeleft+sizeright+1;
+        int sizeOfP1=xKaleft+xKaright+1;
         int remaining= totalSize-sizeOfP1;
         
         int minrq=(n+1)/2;
-        int p2=Math.max(sizeleft,Math.max(sizeright,remaining));
+        int p2=Math.max(remaining,Math.max(xKaleft,xKaright));
         if(p2>=minrq){
             return true;
         }
