9449002ad7aff85406c1bca6ded3b0c900e0069e
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-05-30 12:56:49 +0530

    Time: 1 ms (43.58%), Space: 42.4 MB (29.42%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -13,20 +13,52 @@
  *     }
  * }
  */
-class Solution {
-    public void preOrder(TreeNode root,List<Integer> ans){
-        if(root==null){
-            return;
-        }
-        ans.add(root.val);
-        preOrder(root.left,ans);
-        preOrder(root.right,ans);
+
+
+//Recursive approach
+// class Solution {
+//     public void preOrder(TreeNode root,List<Integer> ans){
+//         if(root==null){
+//             return;
+//         }
+//         ans.add(root.val);
+//         preOrder(root.left,ans);
+//         preOrder(root.right,ans);
         
-    }
+//     }
+    
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//        List<Integer> ans=new ArrayList<>();
+//         preOrder(root,ans);
+//         return ans;
+//     }
+// }
+
+
+//Morris Traversal
+class Solution {
+    
     
     public List<Integer> preorderTraversal(TreeNode root) {
        List<Integer> ans=new ArrayList<>();
-        preOrder(root,ans);
+        TreeNode curr=root;
+        
+        while(curr != null){
+            if(curr.left!=null){
+                TreeNode iop=curr.left;
+                while(iop.right!=null){
+                    iop=iop.right;
+                }
+                iop.right=curr.right;
+                ans.add(curr.val);
+                curr=curr.left;
+            }
+            else{
+                ans.add(curr.val);
+                curr=curr.right;
+                
+            }
+        }
         return ans;
     }
 }
