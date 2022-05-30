4098d601cb2dd59ea46a1f55c800619b165b5199
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-05-30 11:35:22 +0530

    Time: 0 ms (100.00%), Space: 42 MB (57.26%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -13,20 +13,57 @@
  *     }
  * }
  */
+
+//Morris Traversal
 class Solution {
-    public void inOrder(TreeNode root,List<Integer> ans){
-        if(root==null){
-            return;
-        }
-        
-        inOrder(root.left,ans);
-        ans.add(root.val);
-        inOrder(root.right,ans);
-        
-    }
+    
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans=new ArrayList<>();
-        inOrder(root,ans);
+        TreeNode curr=root;
+        
+        while(curr != null){
+            if(curr.left==null){
+                ans.add(curr.val);
+                curr=curr.right;
+            }
+            else{
+                TreeNode iop=curr.left;
+                while((iop.right!=null) && (iop.right!=curr)){
+                    iop=iop.right;
+                }
+                if(iop.right==null){
+                    iop.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    iop.right=null;
+                    ans.add(curr.val);
+                    curr=curr.right;
+                }
+            }
+        }
         return ans;
     }
 }
+
+// Recursive Method
+// class Solution {
+//     public void inOrder(TreeNode root,List<Integer> ans){
+//         if(root==null){
+//             return;
+//         }
+        
+//         inOrder(root.left,ans);
+//         ans.add(root.val);
+//         inOrder(root.right,ans);
+        
+//     }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> ans=new ArrayList<>();
+//         inOrder(root,ans);
+//         return ans;
+//     }
+// }
+
+
+
