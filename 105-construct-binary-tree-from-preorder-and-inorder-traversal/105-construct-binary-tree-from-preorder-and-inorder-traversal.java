cc907478e73465e4972c4e46b4b6572807219fba
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-05-31 15:50:02 +0530

    Time: 2 ms (96.02%), Space: 44.3 MB (53.56%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,31 +15,32 @@
  */
 class Solution {
     
-    public TreeNode build(int[] preorder, int[] inorder,int stp,int enp,int stIn,int enIn){
+    public TreeNode build(int[] preorder, int[] inorder,int stp,int enp,int stIn,int enIn,HashMap<Integer,Integer> hm){
         if(stIn==enIn){
            return new TreeNode(inorder[stIn]); 
         }
-        // else if(stIn>enIn){
-        //     return null;
-        // }
-        TreeNode root=new TreeNode(preorder[stp]);
-        int mid=-1;
-        for(int i=stIn;i<=enIn;i++){
-            if(inorder[i]==preorder[stp]){
-                mid=i;
-                
-            }
+        else if(stIn>enIn){
+            return null;
         }
+        TreeNode root=new TreeNode(preorder[stp]);
+//         int mid=-1;
+//         for(int i=stIn;i<=enIn;i++){
+//             if(inorder[i]==preorder[stp]){
+//                 mid=i;
+                
+//             }
+//         }
+        int mid=hm.get(preorder[stp]);
         int len = mid-stIn;
         if(mid==stIn){
             root.left=null;
         }else{
-            root.left=build(preorder,inorder,stp+1,stp+len,stIn,mid-1);
+            root.left=build(preorder,inorder,stp+1,stp+len,stIn,mid-1,hm);
         }
         if(mid==enIn){
             root.right=null;
         }else{
-            root.right=build(preorder,inorder,stp+len+1,enp,mid+1,enIn);
+            root.right=build(preorder,inorder,stp+len+1,enp,mid+1,enIn,hm);
         }
         
         return root;
@@ -53,7 +54,10 @@ class Solution {
         if(n==0){
             return new TreeNode(inorder[0]);
         }
-        
-        return build(preorder,inorder,0,n,0,n);
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<=n;i++){
+            hm.put(inorder[i],i);
+        }
+        return build(preorder,inorder,0,n,0,n,hm);
     }
 }
