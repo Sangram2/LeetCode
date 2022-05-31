403eb72376e05b048fe38252a5db69f0a1776000
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-05-31 15:54:55 +0530

    Time: 4 ms (54.69%), Space: 44.4 MB (49.04%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,9 +15,9 @@
  */
 class Solution {
     
-    public TreeNode build(int[] preorder, int[] inorder,int stp,int enp,int stIn,int enIn,HashMap<Integer,Integer> hm){
-        if(stIn==enIn){
-           return new TreeNode(inorder[stIn]); 
+    public TreeNode build(int[] preorder,int stp,int enp,int stIn,int enIn,HashMap<Integer,Integer> hm){
+        if(stp==enp){
+           return new TreeNode(preorder[stp]); 
         }
         else if(stIn>enIn){
             return null;
@@ -35,12 +35,12 @@ class Solution {
         if(mid==stIn){
             root.left=null;
         }else{
-            root.left=build(preorder,inorder,stp+1,stp+len,stIn,mid-1,hm);
+            root.left=build(preorder,stp+1,stp+len,stIn,mid-1,hm);
         }
         if(mid==enIn){
             root.right=null;
         }else{
-            root.right=build(preorder,inorder,stp+len+1,enp,mid+1,enIn,hm);
+            root.right=build(preorder,stp+len+1,enp,mid+1,enIn,hm);
         }
         
         return root;
@@ -58,6 +58,6 @@ class Solution {
         for(int i=0;i<=n;i++){
             hm.put(inorder[i],i);
         }
-        return build(preorder,inorder,0,n,0,n,hm);
+        return build(preorder,0,n,0,n,hm);
     }
 }
