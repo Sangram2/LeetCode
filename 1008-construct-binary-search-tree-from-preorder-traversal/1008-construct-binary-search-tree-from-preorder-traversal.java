a842ce00f0e2412a4835a6a5d8df6d803d2d23ad
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-06-03 20:19:13 +0530

    Time: 1 ms (52.83%), Space: 42 MB (52.21%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -13,22 +13,50 @@
  *     }
  * }
  */
+// class Solution {
+//     static int idx=0;
+//     public TreeNode bstFromPreorder(int[] preorder) {
+//         idx=0;
+//         TreeNode ans=construct(preorder,Integer.MIN_VALUE,Integer.MAX_VALUE);
+        
+//         return ans;
+        
+//     }
+//     public TreeNode construct(int[] preorder,Integer st,Integer en){
+//         if(idx<preorder.length){
+//             if(preorder[idx]<en && preorder[idx]>st){
+//                 TreeNode root=new TreeNode(preorder[idx]);
+//                 idx++;
+//                 root.left= construct(preorder,st,root.val);
+//                 root.right=construct(preorder,root.val,en);
+//                 return root;
+//             }
+//             else{
+//                 return null;
+//             }
+
+//         }
+//         else{
+//             return null;
+//         }
+//     }
+// }
 class Solution {
     static int idx=0;
     public TreeNode bstFromPreorder(int[] preorder) {
         idx=0;
-        TreeNode ans=construct(preorder,Integer.MIN_VALUE,Integer.MAX_VALUE);
+        TreeNode ans=construct(preorder,Integer.MAX_VALUE);
         
         return ans;
         
     }
-    public TreeNode construct(int[] preorder,Integer st,Integer en){
+    public TreeNode construct(int[] preorder,Integer en){
         if(idx<preorder.length){
-            if(preorder[idx]<en && preorder[idx]>st){
+            if(preorder[idx]<en){
                 TreeNode root=new TreeNode(preorder[idx]);
                 idx++;
-                root.left= construct(preorder,st,root.val);
-                root.right=construct(preorder,root.val,en);
+                root.left= construct(preorder,root.val);
+                root.right=construct(preorder,en);
                 return root;
             }
             else{
