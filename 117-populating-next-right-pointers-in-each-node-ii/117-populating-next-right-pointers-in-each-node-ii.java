520dca641727c63f4cddb4158932fbed56e56ddf
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-06-04 21:13:55 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (47.20%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -21,38 +21,114 @@ class Node {
 };
 */
 
-// class Solution {
-//     public Node connect(Node root) {
-        
-//     }
-// }
-
 class Solution {
     public Node connect(Node root) {
         if(root==null){
             return null;
         }
         
-        Queue<Node> qu=new LinkedList<>();
-        qu.add(root);
-        qu.add(null);
-        while(qu.size()!=0){
-            Node rem=qu.remove();
-            if(rem!=null){
-                rem.next=qu.peek();
-                if(rem.left!=null){
-                    qu.add(rem.left);
-                }  
-                if(rem.right!=null){
-                    qu.add(rem.right);
-                }
-            }
-                
+        
+        Node curr=root;
+        while(curr!=null){
+            Node h=null;
+            Node t=null;
             
-            else if(rem==null && qu.size()!=0){
-                qu.add(null);
+            
+            if(curr.left!=null){
+                h=t=curr.left;
+                
+                
+                while(curr!=null){
+                    if(curr.right!=null){
+                        t.next=curr.right;
+                        t=t.next;
+                        curr=curr.next;
+                        if(curr!=null && curr.left!=null){
+                            t.next=curr.left;
+                            t=t.next;  
+                        }
+                    }else{
+                        curr=curr.next;
+                        if(curr!=null && curr.left!=null){
+                            t.next=curr.left;
+                            t=t.next;
+                        }
+                    }
+                }
+                curr=h;  
+                
             }
+            
+            
+            else if(curr.right!=null){
+                h=t=curr.right;
+                curr=curr.next;
+                
+                
+                while(curr!=null){
+                    
+                    if(curr.left!=null){
+                        t.next=curr.left;
+                        t=t.next;
+                        
+                        if(curr.right!=null){
+                            t.next=curr.right;
+                            t=t.next;                       
+                        }
+                        curr=curr.next;
+                        
+                    }
+                    
+                    else{
+                        
+                        if(curr.right!=null){
+                            t.next=curr.right;
+                            t=t.next;
+                            curr=curr.next;
+                        }
+                        else{
+                            curr=curr.next;
+                        }
+                    }
+                }
+                curr=h;
+            }
+            
+            else{
+                curr=curr.next;
+            }
+            
         }
         return root;
     }
 }
+
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root==null){
+//             return null;
+//         }
+        
+//         Queue<Node> qu=new LinkedList<>();
+//         qu.add(root);
+//         qu.add(null);
+//         while(qu.size()!=0){
+//             Node rem=qu.remove();
+//             if(rem!=null){
+//                 rem.next=qu.peek();
+//                 if(rem.left!=null){
+//                     qu.add(rem.left);
+//                 }  
+//                 if(rem.right!=null){
+//                     qu.add(rem.right);
+//                 }
+//             }
+                
+            
+//             else if(rem==null && qu.size()!=0){
+//                 qu.add(null);
+//             }
+//         }
+//         return root;
+//     }
+// }
