fc524518cfa742c3ad9e7023242aa34e3802a324
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-06-04 21:31:11 +0530

    Time: 1 ms (73.93%), Space: 44.6 MB (62.96%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -20,89 +20,133 @@ class Node {
     }
 };
 */
-
+//Method 1
 class Solution {
     public Node connect(Node root) {
-        if(root==null){
-            return null;
-        }
-        
-        
-        Node curr=root;
-        while(curr!=null){
-            Node h=null;
-            Node t=null;
-            
-            
-            if(curr.left!=null){
-                h=t=curr.left;
-                
-                
-                while(curr!=null){
-                    if(curr.right!=null){
-                        t.next=curr.right;
-                        t=t.next;
-                        curr=curr.next;
-                        if(curr!=null && curr.left!=null){
-                            t.next=curr.left;
-                            t=t.next;  
-                        }
-                    }else{
-                        curr=curr.next;
-                        if(curr!=null && curr.left!=null){
-                            t.next=curr.left;
-                            t=t.next;
-                        }
+        Node curr = root;
+        Node head = null;
+        Node tail = null;
+
+        while(curr != null){
+            // code for curr on level x, and head-tail on level x + 1
+            while(curr != null){
+                if(curr.left != null){
+                    if(head == null){
+                        head = curr.left;
+                        tail = curr.left;
+                    } else {
+                        tail.next = curr.left;
+                        tail = curr.left;
                     }
                 }
-                curr=h;  
-                
-            }
-            
-            
-            else if(curr.right!=null){
-                h=t=curr.right;
-                curr=curr.next;
-                
-                
-                while(curr!=null){
-                    
-                    if(curr.left!=null){
-                        t.next=curr.left;
-                        t=t.next;
-                        
-                        if(curr.right!=null){
-                            t.next=curr.right;
-                            t=t.next;                       
-                        }
-                        curr=curr.next;
-                        
-                    }
-                    
-                    else{
-                        
-                        if(curr.right!=null){
-                            t.next=curr.right;
-                            t=t.next;
-                            curr=curr.next;
-                        }
-                        else{
-                            curr=curr.next;
-                        }
+
+                if(curr.right != null){
+                    if(head == null){
+                        head = curr.right;
+                        tail = curr.right;
+                    } else {
+                        tail.next = curr.right;
+                        tail = curr.right;
                     }
                 }
-                curr=h;
+
+                curr = curr.next;
             }
-            
-            else{
-                curr=curr.next;
-            }
-            
+
+            curr = head;
+            head = null;
+            tail = null;
         }
+
         return root;
     }
 }
+//method 2
 
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root==null){
+//             return null;
+//         }
+        
+        
+//         Node curr=root;
+//         while(curr!=null){
+//             Node h=null;
+//             Node t=null;
+            
+            
+//             if(curr.left!=null){
+//                 h=t=curr.left;
+                
+//                 while(curr!=null){
+//                     if(curr.right!=null){
+//                         t.next=curr.right;
+//                         t=t.next;
+//                         curr=curr.next;
+//                         if(curr!=null && curr.left!=null){
+//                             t.next=curr.left;
+//                             t=t.next;  
+//                         }
+//                     }else{
+//                         curr=curr.next;
+//                         if(curr!=null && curr.left!=null){
+//                             t.next=curr.left;
+//                             t=t.next;
+//                         }
+//                     }
+//                 }
+                
+//                 curr=h;  
+                
+//             }
+            
+            
+//             else if(curr.right!=null){
+//                 h=t=curr.right;
+//                 curr=curr.next;
+                
+//                 while(curr!=null){
+//                     if(curr.left!=null){
+//                         t.next=curr.left;
+//                         t=t.next;
+                        
+//                         if(curr.right!=null){
+//                             t.next=curr.right;
+//                             t=t.next;                       
+//                         }
+//                         curr=curr.next;
+                        
+//                     }
+                    
+//                     else{
+                        
+//                         if(curr.right!=null){
+//                             t.next=curr.right;
+//                             t=t.next;
+//                             curr=curr.next;
+//                         }
+//                         else{
+//                             curr=curr.next;
+//                         }
+//                     }
+//                 }
+//                 curr=h;
+//             }
+            
+            
+            
+//             else{
+//                 curr=curr.next;
+//             }
+            
+//         }
+//         return root;
+//     }
+// }
+
+
+//method 3
 // class Solution {
 //     public Node connect(Node root) {
 //         if(root==null){
