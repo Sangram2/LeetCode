90f813bf5a36da61687c0268fc8b5f261f120b86
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-06-04 14:56:23 +0530

    Time: 2 ms (22.04%), Space: 44.4 MB (17.45%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -13,26 +13,54 @@
  *     }
  * }
  */
-class Solution {
-    long prev = Long.MIN_VALUE;
-    public boolean isValidBST(TreeNode root) {
-        if(root == null)
-            return true;
+// class Solution {
+//     long prev = Long.MIN_VALUE;
+//     public boolean isValidBST(TreeNode root) {
+//         if(root == null)
+//             return true;
             
-        boolean la = isValidBST(root.left);
-        if(la == false)
-            return false;
+//         boolean la = isValidBST(root.left);
+//         if(la == false)
+//             return false;
         
         
-        if(prev >= root.val)
-            return false;
-        prev = root.val;
+//         if(prev >= root.val)
+//             return false;
+//         prev = root.val;
         
-        boolean ra = isValidBST(root.right);
-        if(ra == false)
-            return false;
+//         boolean ra = isValidBST(root.right);
+//         if(ra == false)
+//             return false;
         
         
-        return true;
+//         return true;
+//     }
+// }
+class Solution {
+    class Pair {
+        boolean isBST = true;
+        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
+    }
+    
+    public boolean isValidBST(TreeNode root) {
+        Pair rp = helper(root);
+        return rp.isBST;
+    }
+    
+    public Pair helper(TreeNode node){
+        if(node == null){
+            return new Pair();
+        }
+        
+        Pair lp = helper(node.left);
+        Pair rp = helper(node.right);
+        
+        Pair mp = new Pair();
+        mp.min = Math.min(node.val, Math.min(lp.min, rp.min));
+        mp.max = Math.max(node.val, Math.max(lp.max, rp.max));
+        mp.isBST = lp.isBST & rp.isBST && node.val > lp.max && node.val < rp.min;
+        
+        return mp;
     }
 }
