b61fe79f9cba309d7bcf388937f0d6569cdbbb85
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-06-06 21:02:46 +0530

    Added solution - LeetHub

--- Binary Tree to CDLL - GFG/binary-tree-to-cdll.java
+++ Binary Tree to CDLL - GFG/binary-tree-to-cdll.java
@@ -139,59 +139,105 @@ class Node{
     }
 }
 */
+//Method 1
+// class Solution
+// { 
+//     class pair{
+//         Node head;
+//         Node tail;
+        
+//         pair(){
+            
+//         }
+//         pair(Node head,Node tail){
+//             this.head=head;
+//             this.tail=tail;
+//         }
+//     }
+//     Node bTreeToClist(Node root)
+//     {
+//         pair ll=helper(root);
+//         ll.head.left=ll.tail;
+//         ll.tail.right=ll.head;
+//         return ll.head;
+//     }
+//     pair helper(Node node){
+//         if(node==null){
+//             return null;
+//         }
+//         if(node.left==null && node.right==null){
+//             return new pair(node,node);
+//         }
+//         pair lp=helper(node.left);
+//         pair rp=helper(node.right);
+//         pair me=new pair();
+//         if(lp!=null){
+//             lp.tail.right=node;
+//             node.left=lp.tail;
+//             me.head=lp.head;
+//         }
+//         else{
+//             me.head=node;
+//         }
+//         if(rp!=null){
+//             node.right=rp.head;
+//             rp.head.left=node;
+//             me.tail=rp.tail;
+//             return me; 
+//         }
+//         else{
+//             me.tail=node;
+//         }
+//         return me;
+        
+        
+        
+//     }
+    
+// }
+    
+//Method 2
 class Solution
 { 
-    class pair{
-        Node head;
-        Node tail;
-        
-        pair(){
-            
-        }
-        pair(Node head,Node tail){
-            this.head=head;
-            this.tail=tail;
-        }
-    }
+    //Function to convert binary tree into circular doubly linked list.
     Node bTreeToClist(Node root)
     {
-        pair ll=helper(root);
-        ll.head.left=ll.tail;
-        ll.tail.right=ll.head;
-        return ll.head;
+        return helper(root);
     }
-    pair helper(Node node){
-        if(node==null){
+
+    Node helper(Node node){
+        if(node == null){
             return null;
         }
-        if(node.left==null && node.right==null){
-            return new pair(node,node);
-        }
-        pair lp=helper(node.left);
-        pair rp=helper(node.right);
-        pair me=new pair();
-        if(lp!=null){
-            lp.tail.right=node;
-            node.left=lp.tail;
-            me.head=lp.head;
-        }
-        else{
-            me.head=node;
-        }
-        if(rp!=null){
-            node.right=rp.head;
-            rp.head.left=node;
-            me.tail=rp.tail;
-            return me; 
-        }
-        else{
-            me.tail=node;
-        }
-        return me;
-        
-        
-        
+
+        Node lhead = helper(node.left);
+        Node rhead = helper(node.right);
+
+        Node onl = node;
+        onl.left = onl.right = onl;
+
+        Node s1 = concat(lhead, onl);
+        Node s2 = concat(s1, rhead);
+
+        return s2;
     }
-    
+
+    Node concat(Node h1, Node h2){
+        if(h1 == null){
+            return h2;
+        } else if(h2 == null){
+            return h1;
+        }
+
+        Node t1 = h1.left;
+        Node t2 = h2.left;
+
+        t1.right = h2;
+        h2.left = t1;
+
+        t2.right = h1;
+        h1.left = t2;
+
+        return h1;
+    }
 }
-    
