30934fe7e0d008a2ac36ccedaef43381e75f9df0
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-06-06 16:18:44 +0530

    Time: 1 ms (67.09%), Space: 43.3 MB (11.98%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -13,43 +13,110 @@
  *     }
  * }
  */
-class Solution {
-    public void flatten(TreeNode root) {
-        if(root==null)
-            return;
+// Method 1
+// class Solution {
+//     public void flatten(TreeNode root) {
+//         if(root==null)
+//             return;
         
-        TreeNode l=helper(root.left);
-        TreeNode r=helper(root.right);
-        if(l!=null){
-            TreeNode temp=l;
+//         TreeNode l=helper(root.left);
+//         TreeNode r=helper(root.right);
+//         if(l!=null){
+//             TreeNode temp=l;
         
-            while(temp.right!=null){
-                temp=temp.right;
-            }
-            temp.right=r;
-        }else{
-            l=r;
-        }
+//             while(temp.right!=null){
+//                 temp=temp.right;
+//             }
+//             temp.right=r;
+//         }else{
+//             l=r;
+//         }
         
-        root.right=null;
-        root.left=null;
-        root.right=l;
+//         root.right=null;
+//         root.left=null;
+//         root.right=l;
         
-    }
-    public TreeNode helper(TreeNode root){
-        if(root==null){
-            return null;
-        }
+//     }
+//     public TreeNode helper(TreeNode root){
+//         if(root==null){
+//             return null;
+//         }
         
-        TreeNode node=new TreeNode(root.val);
+//         TreeNode node=new TreeNode(root.val);
         
-        node.right=helper(root.left);
-        TreeNode temp=node;
-        while(temp.right!=null){
-            temp=temp.right;
-        }
-        temp.right=helper(root.right);
-        return node;
+//         node.right=helper(root.left);
+//         TreeNode temp=node;
+//         while(temp.right!=null){
+//             temp=temp.right;
+//         }
+//         temp.right=helper(root.right);
+//         return node;
        
+//     }
+// }
+
+// Method 2
+class Solution {
+    class Pair {
+        TreeNode head;
+        TreeNode tail;
+
+        Pair(){
+
+        }
+
+        Pair(TreeNode head, TreeNode tail){
+            this.head = head;
+            this.tail = tail;
+        }
     }
+
+    public void flatten(TreeNode root) {
+        if(root != null){
+            helper(root);
+        }
+    }
+
+    public Pair helper(TreeNode node){
+        if(node.left != null && node.right != null){
+            Pair lp = helper(node.left);
+            Pair rp = helper(node.right);
+
+            Pair mp = new Pair();
+
+            mp.head = node;
+            mp.head.left = null;
+            mp.head.right = lp.head;
+            lp.tail.right = rp.head;
+            mp.tail = rp.tail;
+
+            return mp;
+        } else if(node.left != null){
+            Pair lp = helper(node.left);
+
+            Pair mp = new Pair();
+
+            mp.head = node;
+            mp.head.left = null;
+            mp.head.right = lp.head;
+            mp.tail = lp.tail;
+
+            return mp;
+        } else if(node.right != null){
+            Pair rp = helper(node.right);
+
+            Pair mp = new Pair();
+
+            mp.head = node;
+            mp.head.left = null;
+            mp.head.right = rp.head;
+            mp.tail = rp.tail;
+
+            return mp;
+        } else {
+            Pair mp = new Pair(node, node);
+            return mp;
+        }
+    }
+
 }
