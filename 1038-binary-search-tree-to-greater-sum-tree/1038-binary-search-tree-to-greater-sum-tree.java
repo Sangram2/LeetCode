9637dcef6897b2246915dfdf941df91f7739f49b
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-06-10 15:33:28 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (37.34%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -14,49 +14,74 @@
  * }
  */
 class Solution {
+    int sum=0;
     public TreeNode bstToGst(TreeNode root) {
         if(root==null){
             return null;
         }
-        int sum=findSum(root);
-        
-        TreeNode curr=root;
-        while(curr!=null){
-            if(curr.left==null){
-                int data=curr.val;
-                curr.val=sum;
-                sum-=data;
-                curr=curr.right;
-                
-            }
-            else{
-                TreeNode temp=curr.left;
-                while(temp.right !=null && temp.right!=curr){
-                    temp=temp.right;
-                }
-                if(temp.right==null){
-                    temp.right=curr;
-                    curr=curr.left;
-                }
-                else{
-                    int data=curr.val;
-                    curr.val=sum;
-                   temp.right=null;
-                    sum-=data;
-                    curr=curr.right;
-                    
-                    
-                }
-            }
-        }
+        helper(root);
         return root;
     }
-     public int findSum(TreeNode root){
+    public void helper(TreeNode root){
         if(root==null){
-            return 0;
+            return;
         }
-        int left=findSum(root.left);
-        int right=findSum(root.right);
-        return root.val+left+right;
+        helper(root.right);
+        
+        root.val+=sum;
+        sum=root.val;
+        
+        helper(root.left);
     }
+    
 }
+
+
+//Method 2
+// class Solution {
+//     public TreeNode bstToGst(TreeNode root) {
+//         if(root==null){
+//             return null;
+//         }
+//         int sum=findSum(root);
+        
+//         TreeNode curr=root;
+//         while(curr!=null){
+//             if(curr.left==null){
+//                 int data=curr.val;
+//                 curr.val=sum;
+//                 sum-=data;
+//                 curr=curr.right;
+                
+//             }
+//             else{
+//                 TreeNode temp=curr.left;
+//                 while(temp.right !=null && temp.right!=curr){
+//                     temp=temp.right;
+//                 }
+//                 if(temp.right==null){
+//                     temp.right=curr;
+//                     curr=curr.left;
+//                 }
+//                 else{
+//                     int data=curr.val;
+//                     curr.val=sum;
+//                    temp.right=null;
+//                     sum-=data;
+//                     curr=curr.right;
+                    
+                    
+//                 }
+//             }
+//         }
+//         return root;
+//     }
+//      public int findSum(TreeNode root){
+//         if(root==null){
+//             return 0;
+//         }
+//         int left=findSum(root.left);
+//         int right=findSum(root.right);
+//         return root.val+left+right;
+//     }
+// }
