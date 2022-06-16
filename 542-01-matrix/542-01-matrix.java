fb55fad272d9fccaa67654301fee97abc191cdb5
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-06-16 16:36:16 +0530

    Time: 50 ms (7.79%), Space: 85.5 MB (5.34%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -12,8 +12,8 @@ class Solution {
     static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
     public int[][] updateMatrix(int[][] mat) {
         
-        int [][]ans =new int[mat.length][mat[0].length];
-        boolean [][]vis =new boolean[mat.length][mat[0].length];
+        
+        int [][]vis =new int[mat.length][mat[0].length];
         
         Queue<pair> qu = new LinkedList<>();
         for(int i=0;i<mat.length;i++){
@@ -21,28 +21,29 @@ class Solution {
                 if(mat[i][j]==0){
                     qu.add(new pair(i,j,0));
                 }
+                vis[i][j]=-1;
             }
         }
         
         while(qu.size()!=0){
             pair rem=qu.remove();
-            if(vis[rem.x][rem.y]==true){
+            if(vis[rem.x][rem.y]!=-1){
                 continue;
             }
-            vis[rem.x][rem.y]=true;
-            ans[rem.x][rem.y]=rem.dis;
+            vis[rem.x][rem.y]=rem.dis;
+            
             
             for(int i=0;i<dir.length;i++){
                 int r=rem.x+dir[i][0];
                 int c= rem.y+dir[i][1];
                 
-                if(r>=0 && c>=0 && r<mat.length && c<mat[0].length && vis[r][c]==false && mat[r][c]==1){
+                if(r>=0 && c>=0 && r<mat.length && c<mat[0].length && vis[r][c]==-1 && mat[r][c]==1){
                     qu.add(new pair(r,c,rem.dis+1));
                 }
             }
             
         }
-        return ans;
+        return vis;
     }
 }
 
