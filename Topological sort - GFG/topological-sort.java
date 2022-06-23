4589a715219ccfe5f2ee315263c297af1028e6df
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-06-23 20:02:07 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.java
+++ Topological sort - GFG/topological-sort.java
@@ -90,6 +90,8 @@ class Solution
                 }
             }
         }
+        
+        //This check tells about if there is a cycle or not...
         boolean isCyclePresent = false;
         if(count!=V){
             isCyclePresent = true;
@@ -98,3 +100,46 @@ class Solution
         return ans;
     }
 }
+
+
+//Sir  ka  code
+
+
+// class Solution
+// {
+//     //Function to return list containing vertices in Topological order. 
+//     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+//     {
+//         // add your code here
+//         int[] tsort = new int[V];
+        
+//         int[] inDegree = new int[V];
+//         for(int v = 0; v < V; v++){
+//             for(int n: adj.get(v)){
+//                 inDegree[n]++;
+//             }
+//         }
+        
+//         ArrayDeque<Integer> queue = new ArrayDeque<>();
+//         for(int v = 0; v < V; v++){
+//             if(inDegree[v] == 0){
+//                 queue.add(v);
+//             }
+//         }
+        
+//         int idx = 0;
+//         while(queue.size() > 0){
+//             int v = queue.remove();
+//             tsort[idx++] = v;
+            
+//             for(int n: adj.get(v)){
+//                 inDegree[n]--;
+//                 if(inDegree[n] == 0){
+//                     queue.add(n);
+//                 }
+//             }
+//         }
+        
+//         return tsort;
+//     }
+// }
