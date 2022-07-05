98f89581331f231135612878a18be85fb086b9fd
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-05 15:04:55 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        Arrays.sort(arr,(a,b)->{
+            return b.profit-a.profit;
+        });
+        
+        int count = 0;
+        int sum = 0;
+        parent = new int [101];
+        for(int i=0;i<101;i++){
+            parent[i] = i;
+        }
+        for(Job j : arr){
+            int empty = find(j.deadline);
+            if(empty!=0){
+                count++;
+                sum+=j.profit;
+                
+                parent[empty] = find(empty-1);
+            }
+        }
+        return new int[]{count,sum};
+        
+        
+    }
+    int[] parent;
+    int find(int x){
+        if(parent[x] == x){
+            return x;
+        }else{
+            parent[x] = find(parent[x]);
+            return parent[x];
+        }
+    }
+}
+
+/*
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+*/
