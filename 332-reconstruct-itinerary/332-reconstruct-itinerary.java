16edfb9a8114f9eae77eca7be31efa9d431d991e
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-07 15:49:12 +0530

    Time: 10 ms (61.39%), Space: 49.7 MB (75.12%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.java
@@ -1,3 +1,37 @@
+// class Solution {
+//     HashMap<String,PriorityQueue<String>> adj;
+//     public List<String> findItinerary(List<List<String>> tickets) {
+//         adj = new HashMap<>();
+//         for(List<String> ticket : tickets){
+//             String from = ticket.get(0);
+//             String to = ticket.get(1);
+//             if(!adj.containsKey(from)){
+//                 adj.put(from, new PriorityQueue<>());
+//             }
+//             adj.get(from).add(to);
+//         }
+        
+//         List<String> res = new ArrayList<>();
+//         dfs("JFK",res);
+//         return res;
+//     }
+    
+//     void dfs(String v,List<String> res){
+//         if(adj.containsKey(v)==false || adj.get(v).size()==0){
+//             res.add(0,v);
+//             return;
+            
+//         }
+        
+//         while(adj.get(v).size()>0){
+//             String newPlace = adj.get(v).remove();
+//             dfs(newPlace,res);
+//         }
+//         res.add(0,v);
+//     }
+
+// }
+
 class Solution {
     HashMap<String,PriorityQueue<String>> adj;
     public List<String> findItinerary(List<List<String>> tickets) {
@@ -11,14 +45,14 @@ class Solution {
             adj.get(from).add(to);
         }
         
-        List<String> res = new ArrayList<>();
+        LinkedList<String> res = new LinkedList<>();
         dfs("JFK",res);
         return res;
     }
     
-    void dfs(String v,List<String> res){
+    void dfs(String v,LinkedList<String> res){
         if(adj.containsKey(v)==false || adj.get(v).size()==0){
-            res.add(0,v);
+            res.addFirst(v);
             return;
             
         }
@@ -27,7 +61,7 @@ class Solution {
             String newPlace = adj.get(v).remove();
             dfs(newPlace,res);
         }
-        res.add(0,v);
+        res.addFirst(v);
     }
 
 }
