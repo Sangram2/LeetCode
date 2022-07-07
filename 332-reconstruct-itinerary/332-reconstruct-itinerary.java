742791f7a944258c075f97504eccaf5993cd9544
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-07 15:44:30 +0530

    Time: 5 ms (98.40%), Space: 43.2 MB (85.79%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.java
@@ -0,0 +1,33 @@
+class Solution {
+    HashMap<String,PriorityQueue<String>> adj;
+    public List<String> findItinerary(List<List<String>> tickets) {
+        adj = new HashMap<>();
+        for(List<String> ticket : tickets){
+            String from = ticket.get(0);
+            String to = ticket.get(1);
+            if(!adj.containsKey(from)){
+                adj.put(from, new PriorityQueue<>());
+            }
+            adj.get(from).add(to);
+        }
+        
+        List<String> res = new ArrayList<>();
+        dfs("JFK",res);
+        return res;
+    }
+    
+    void dfs(String v,List<String> res){
+        if(adj.containsKey(v)==false || adj.get(v).size()==0){
+            res.add(0,v);
+            return;
+            
+        }
+        
+        while(adj.get(v).size()>0){
+            String newPlace = adj.get(v).remove();
+            dfs(newPlace,res);
+        }
+        res.add(0,v);
+    }
+
+}
