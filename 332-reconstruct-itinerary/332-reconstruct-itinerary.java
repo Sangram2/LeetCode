c3d73bb938caf58ece5ab8e1a88ba1400826c97b
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-07 15:50:14 +0530

    Time: 13 ms (37.16%), Space: 49.6 MB (75.12%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.java
@@ -1,37 +1,3 @@
-class Solution {
-    HashMap<String,PriorityQueue<String>> adj;
-    public List<String> findItinerary(List<List<String>> tickets) {
-        adj = new HashMap<>();
-        for(List<String> ticket : tickets){
-            String from = ticket.get(0);
-            String to = ticket.get(1);
-            if(!adj.containsKey(from)){
-                adj.put(from, new PriorityQueue<>());
-            }
-            adj.get(from).add(to);
-        }
-        
-        List<String> res = new ArrayList<>();
-        dfs("JFK",res);
-        return res;
-    }
-    
-    void dfs(String v,List<String> res){
-        if(adj.containsKey(v)==false || adj.get(v).size()==0){
-            res.add(0,v);
-            return;
-            
-        }
-        
-        while(adj.get(v).size()>0){
-            String newPlace = adj.get(v).remove();
-            dfs(newPlace,res);
-        }
-        res.add(0,v);
-    }
-
-}
-
 // class Solution {
 //     HashMap<String,PriorityQueue<String>> adj;
 //     public List<String> findItinerary(List<List<String>> tickets) {
@@ -45,14 +11,14 @@ class Solution {
 //             adj.get(from).add(to);
 //         }
         
-//         LinkedList<String> res = new LinkedList<>();
+//         List<String> res = new ArrayList<>();
 //         dfs("JFK",res);
 //         return res;
 //     }
     
-//     void dfs(String v,LinkedList<String> res){
+//     void dfs(String v,List<String> res){
 //         if(adj.containsKey(v)==false || adj.get(v).size()==0){
-//             res.addFirst(v);
+//             res.add(0,v);
 //             return;
             
 //         }
@@ -61,7 +27,41 @@ class Solution {
 //             String newPlace = adj.get(v).remove();
 //             dfs(newPlace,res);
 //         }
-//         res.addFirst(v);
+//         res.add(0,v);
 //     }
 
 // }
+
+class Solution {
+    HashMap<String,PriorityQueue<String>> adj;
+    public List<String> findItinerary(List<List<String>> tickets) {
+        adj = new HashMap<>();
+        for(List<String> ticket : tickets){
+            String from = ticket.get(0);
+            String to = ticket.get(1);
+            if(!adj.containsKey(from)){
+                adj.put(from, new PriorityQueue<>());
+            }
+            adj.get(from).add(to);
+        }
+        
+        LinkedList<String> res = new LinkedList<>();
+        dfs("JFK",res);
+        return res;
+    }
+    
+    void dfs(String v,LinkedList<String> res){
+        if(adj.containsKey(v)==false || adj.get(v).size()==0){
+            res.addFirst(v);
+            return;
+            
+        }
+        
+        while(adj.get(v).size()>0){
+            String newPlace = adj.get(v).remove();
+            dfs(newPlace,res);
+        }
+        res.addFirst(v);
+    }
+
+}
