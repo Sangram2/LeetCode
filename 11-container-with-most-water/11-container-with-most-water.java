bb73116faf7fd5e52258e82a7164499a9baf9b9d
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-18 13:45:01 +0530

    Time: 3 ms (97.21%), Space: 81.4 MB (23.74%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -21,3 +21,26 @@ class Solution {
         return ans;
     }
 }
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int ans = 0;
+//         int st = 0;
+//         int end = height.length-1;
+//         while(st<end){
+//             int left = height[st];
+//             int right = height[end];
+//             int diff = end-st;
+//             int h = Math.min(left,right);
+//             int area = h*diff;
+//             ans = Math.max(area,ans);
+//             if(left<right){
+//                 st++;
+//             }
+//             else{
+//                 end--;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// }
