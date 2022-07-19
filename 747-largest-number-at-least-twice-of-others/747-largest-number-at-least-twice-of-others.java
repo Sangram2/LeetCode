89ac5861c4b14f7b840a9f6b8306a683fe5f8463
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-19 17:00:18 +0530

    Time: 1 ms (82.77%), Space: 42 MB (54.88%) - LeetHub

--- 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.java
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.java
@@ -1,29 +1,52 @@
+// class Solution {
+//     public int dominantIndex(int[] nums) {
+//         int idx1 = -1;
+//         int idx2 = -1;
+//         int max1 = Integer.MIN_VALUE;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]>max1){
+//                 max1 = nums[i];
+//                 idx1 = i;
+//             }
+//         }
+//         int max2 = Integer.MIN_VALUE;
+//         for(int i =0;i<nums.length;i++){
+//             if(i==idx1){
+//                 continue;
+//             }
+//             else if(nums[i]>max2){
+//                 max2 = nums[i];
+                
+//             }
+//         }
+        
+//         if(max1>=2*max2){
+//             return idx1;
+//         }else{
+//             return -1;
+//         }
+//     }
+// }
+
+
 class Solution {
     public int dominantIndex(int[] nums) {
         int idx1 = -1;
         int idx2 = -1;
-        int max1 = Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>max1){
-                max1 = nums[i];
+        
+        for(int i = 0; i < nums.length; i++){
+            if(idx1 == -1 || nums[i] >= nums[idx1]){
+                idx2 = idx1;
                 idx1 = i;
-            }
-        }
-        int max2 = Integer.MIN_VALUE;
-        for(int i =0;i<nums.length;i++){
-            if(i==idx1){
-                continue;
-            }
-            else if(nums[i]>max2){
-                max2 = nums[i];
-                
+            } else if(idx2 == -1 || nums[i] >= nums[idx2]){
+                 idx2 = i;
             }
         }
         
-        if(max1>=2*max2){
+        if(nums[idx1] >= 2 * nums[idx2]){
             return idx1;
-        }else{
+        } else {
             return -1;
-        }
+        } 
     }
 }
