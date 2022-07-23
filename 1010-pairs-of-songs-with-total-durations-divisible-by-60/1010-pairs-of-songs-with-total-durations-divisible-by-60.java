b00f5e98c1bae70cfcefa2e36b3141f30aed970f
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-23 10:34:02 +0530

    Time: 6 ms (56.47%), Space: 53.8 MB (70.05%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,21 +1,45 @@
+// class Solution {
+//     public int numPairsDivisibleBy60(int[] time) {
+//         int count = 0;
+//         HashMap<Integer,Integer> hm = new HashMap<>();
+//         for(int song:time){
+//             int add = song%60;
+//             int req = (60-(song%60))%60;
+            
+//             if(hm.containsKey(req)){
+//                 count += hm.get(req); 
+//             }
+//             if(hm.containsKey(add)){
+//                 hm.put(add,hm.get(add)+1);
+//             }else{ 
+//                 hm.put(add,1);
+//             }
+            
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
+        int[] rem = new int[60];
         int count = 0;
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int song:time){
-            int add = song%60;
-            int req = (60-(song%60))%60;
+        for(int t:time){
+            int key = t%60;
             
-            if(hm.containsKey(req)){
-                count += hm.get(req); 
+            if(key==0){
+                count+=rem[0];
             }
-            if(hm.containsKey(add)){
-                hm.put(add,hm.get(add)+1);
-            }else{ 
-                hm.put(add,1);
+            else{
+                count += rem[60-key];
+                  
             }
             
+            rem[key]++;
         }
+        
+        
         return count;
+        
     }
 }
