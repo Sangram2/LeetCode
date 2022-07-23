8f2badd3903224fbdb0002335afbf1482268bcd0
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-23 11:50:45 +0530

    Time: 14 ms (10.81%), Space: 45.7 MB (10.88%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,25 +1,65 @@
-class Solution {
+// class Solution {
     
-    public boolean carPooling(int[][] trips, int capacity) {
-        int[] arr = new int[1001];
+//     public boolean carPooling(int[][] trips, int capacity) {
+//         int[] arr = new int[1001];
         
-        for(int[] trip : trips){
-            int to = trip[1];
-            int from = trip[2];
-            int np = trip[0];
+//         for(int[] trip : trips){
+//             int to = trip[1];
+//             int from = trip[2];
+//             int np = trip[0];
             
-            arr[to] += np;
-            arr[from] -= np;
-        }
+//             arr[to] += np;
+//             arr[from] -= np;
+//         }
         
-        int cap = 0;
-        for(int i = 0;i<1001;i++){
-            cap+=arr[i];
-            if(cap>capacity){
-                return false;
+//         int cap = 0;
+//         for(int i = 0;i<1001;i++){
+//             cap+=arr[i];
+//             if(cap>capacity){
+//                 return false;
+//             }
+            
+//         }
+//         return true;
+//     }
+// }
+
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> stops = new ArrayList<>();
+        
+        for(int[] trip: trips){
+            int people = trip[0];
+            int from = trip[1];
+            int to = trip[2];
+            
+            if(map.containsKey(from) == false){
+                map.put(from, +people);
+                stops.add(from);
+            } else {
+                map.put(from, map.get(from) + people);
             }
             
+            if(map.containsKey(to) == false){
+                map.put(to, -people);
+                stops.add(to);
+            } else {
+                map.put(to, map.get(to) - people);
+            }
         }
+        
+        Collections.sort(stops);
+        int pic = 0;
+        for(int stop: stops){
+            int delta = map.get(stop);
+            pic += delta;
+            
+            if(pic > capacity){
+                return false;
+            }
+        }
+        
         return true;
     }
 }
