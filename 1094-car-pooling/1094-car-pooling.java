daeb0697723b8eb101a4853c9254e50c729168c9
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-23 22:53:05 +0530

    Time: 4 ms (71.20%), Space: 42.4 MB (83.60%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -64,6 +64,51 @@
 //     }
 // }
 
+// class Solution {
+    
+//     class Pair implements Comparable<Pair> {
+//         int time;
+//         int delta;
+        
+//         Pair(int time, int delta){
+//             this.time = time;
+//             this.delta = delta;
+//         }
+        
+//         public int compareTo(Pair o){
+//             if(this.time != o.time){
+//                 return this.time - o.time;
+//             } else {
+//                 return this.delta-o.delta;
+//             }
+//         }
+//     }
+    
+//     public boolean carPooling(int[][] trips, int capacity) {
+//         int[] map = new int[1001];
+//         PriorityQueue<Pair> pq = new PriorityQueue<>();
+        
+//         for(int[] trip: trips){
+//             pq.add(new Pair(trip[1], +trip[0]));
+//             pq.add(new Pair(trip[2], -trip[0]));
+//         }
+        
+//         int pic = 0;
+//         while(pq.size() > 0){
+//             Pair rem = pq.remove();
+//             pic += rem.delta;
+            
+//             if(pic > capacity){
+//                 return false;
+//             } else if(pic < 0){
+//                 pic = 0;
+//             }
+//         }
+        
+//         return true;
+//     }
+// }
+
 class Solution {
     
     class Pair implements Comparable<Pair> {
@@ -79,7 +124,13 @@ class Solution {
             if(this.time != o.time){
                 return this.time - o.time;
             } else {
-                return this.delta-o.delta;
+                if(this.delta < 0){
+                    return -1;
+                } else if(o.delta < 0){
+                    return +1;
+                } else {
+                    return 0;
+                }
             }
         }
     }
