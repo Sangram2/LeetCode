c094d35747ec0313af4bef7d9ffd486c39f2a355
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-26 15:20:58 +0530

    Time: 6 ms (49.90%), Space: 52.4 MB (91.43%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -23,9 +23,7 @@ class Solution {
             }
         }
         
-        for(int ele: max){
-            System.out.print(ele+" ");
-        }
+        
         int ans =0;
         for(int i=0;i<nums.length-1;i++){
             if(max[i]<=min[i+1]){
