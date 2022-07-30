a3ed726f0dc977cc6d56ca817615e66bf5f06cf4
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-30 20:50:09 +0530

    Time: 85 ms (74.00%), Space: 96.4 MB (56.01%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,3 +1,39 @@
+// class Solution {
+//     public int findMinArrowShots(int[][] points) {
+//         Arrays.sort(points, (a, b) -> {
+//             if(a[0] > b[0]){
+//                 return 1;
+//             } else if(a[0] == b[0]){
+//                 return 0;
+//             } else {
+//                 return -1;
+//             }
+//         });
+        
+//         int ans = 0;
+//         int end = 0;
+//         for(int i = 0;i<points.length;i++){
+//            if(i==0){
+//                ans++;
+//                end = points[i][1];
+//                continue;
+               
+//            } else{
+//                int st = points[i][0];
+//                if(st<=end){
+//                    end = Math.min(end,points[i][1]);
+//                }
+//                else{
+//                    ans++;
+//                    end = points[i][1];
+//                }
+               
+//            }
+            
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int findMinArrowShots(int[][] points) {
         Arrays.sort(points, (a, b) -> {
@@ -10,27 +46,17 @@ class Solution {
             }
         });
         
-        int ans = 0;
-        int end = 0;
-        for(int i = 0;i<points.length;i++){
-           if(i==0){
-               ans++;
-               end = points[i][1];
-               continue;
-               
-           } else{
-               int st = points[i][0];
-               if(st<=end){
-                   end = Math.min(end,points[i][1]);
-               }
-               else{
-                   ans++;
-                   end = points[i][1];
-               }
-               
-           }
-            
+        int count = 0;
+        int end = Integer.MAX_VALUE;
+        for(int[] p: points){
+            if(p[0] > end){
+                count++;
+                end = p[1];
+            } else {
+                end = Math.min(end, p[1]);
+            }
         }
-        return ans;
+        
+        return count + 1;
     }
 }
