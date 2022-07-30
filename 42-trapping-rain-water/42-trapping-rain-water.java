262299e1cfd4314b6015a6c81d02ead2d010a81f
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-07-30 12:46:17 +0530

    Time: 1 ms (99.74%), Space: 49.9 MB (8.89%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,25 +1,48 @@
+// class Solution {
+//     public int trap(int[] height) {
+//         //you cannot use Integer.MIN_VALUE to initialize l_max,r_max and if you do so then there will be a int overflow at line 12 and 18.
+//         int l_max=0;  
+//         int r_max=0;
+//         int n=height.length;
+//         int low=0,high=n-1;
+//         int aow=0;
+        
+//         while(low<=high){
+//             if(l_max<=r_max){
+//                 aow+=Math.max(0,l_max-height[low]);
+//                 l_max=Math.max(l_max,height[low]);
+//                 low++;
+                
+//             }
+//             else{
+//                 aow+=Math.max(0,r_max-height[high]);
+//                 r_max=Math.max(r_max,height[high]);
+//                 high--;
+//             }
+//         }
+//         return aow;
+//     }
+// }
+
 class Solution {
     public int trap(int[] height) {
-        //you cannot use Integer.MIN_VALUE to initialize l_max,r_max and if you do so then there will be a int overflow at line 12 and 18.
-        int l_max=0;  
-        int r_max=0;
-        int n=height.length;
-        int low=0,high=n-1;
-        int aow=0;
-        
-        while(low<=high){
-            if(l_max<=r_max){
-                aow+=Math.max(0,l_max-height[low]);
-                l_max=Math.max(l_max,height[low]);
-                low++;
-                
-            }
-            else{
-                aow+=Math.max(0,r_max-height[high]);
-                r_max=Math.max(r_max,height[high]);
-                high--;
-            }
+        int[] left_arr = new int[height.length];
+        int[] right_arr = new int[height.length];
+        int max = -1;
+        int ans = 0;
+        for(int i = 0;i<height.length;i++){
+            max=Math.max(max,height[i]);
+            left_arr[i] = max;
         }
-        return aow;
+        max = -1;
+        for(int i = height.length-1;i>=0;i--){
+             max=Math.max(max,height[i]);
+            right_arr[i] = max;
+        }
+        
+        for(int i = 0;i<height.length;i++){
+            ans+= Math.min(left_arr[i],right_arr[i])-height[i];
+        }
+        return ans;
     }
 }
