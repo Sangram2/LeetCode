b2213a9b626cbd7d9b81b4ac3937ecfd927c9469
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-01 09:31:37 +0530

    Time: 298 ms (8.29%), Space: 153.8 MB (31.68%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,43 +1,84 @@
+// class Solution {
+//     public int[] maxSlidingWindow(int[] nums, int k) {
+//         int[] ngtr = find(nums);
+        
+//         int[] res = new int[nums.length-k+1];
+//         int idx = 0;
+         
+//         for(int i = 0;i+k<=nums.length;i++){
+//             int itr = i;
+//             int ans = 0; 
+//             while(itr<i+k){
+//                 ans = nums[itr];
+                
+//                 itr = ngtr[itr];
+//             }
+          
+//             res[idx++] = ans;
+//         } 
+        
+//        return res;
+        
+        
+//     }
+    
+//     public int[] find(int[] nums){
+//         Stack<Integer> st = new Stack<>();
+//         int[] ngtr = new int[nums.length];
+//         st.push(0);
+//         for(int i=1;i<nums.length;i++){
+//             while(st.size()!=0 && nums[i]>nums[st.peek()]){
+//                 int idx = st.pop();
+//                 ngtr[idx] = i;
+//             }
+//             st.push(i);
+//         }
+        
+//         while(st.size()!=0){
+//             ngtr[st.pop()] = nums.length+1;
+//         }
+        
+//         return ngtr;
+//     }
+// }
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] ngtr = find(nums);
-        
-        int[] res = new int[nums.length-k+1];
-        int idx = 0;
-         
-        for(int i = 0;i+k<=nums.length;i++){
-            int itr = i;
-            int ans = 0; 
-            while(itr<i+k){
-                ans = nums[itr];
-                
-                itr = ngtr[itr];
-            }
-          
-            res[idx++] = ans;
-        } 
-        
-       return res;
-        
-        
-    }
-    
-    public int[] find(int[] nums){
+        int[] nge = new int[nums.length];
         Stack<Integer> st = new Stack<>();
-        int[] ngtr = new int[nums.length];
-        st.push(0);
-        for(int i=1;i<nums.length;i++){
-            while(st.size()!=0 && nums[i]>nums[st.peek()]){
-                int idx = st.pop();
-                ngtr[idx] = i;
+        
+        st.push(nums.length - 1);
+        nge[nums.length - 1] = nums.length;
+        
+        for(int i = nums.length - 2; i >= 0; i--){
+            int val = nums[i];
+            while(st.size() > 0 && val >= nums[st.peek()]){
+                st.pop();
             }
+            
+            if(st.size() == 0){
+                nge[i] = nums.length;
+            } else {
+                nge[i] = st.peek();
+            }
+            
             st.push(i);
         }
         
-        while(st.size()!=0){
-            ngtr[st.pop()] = nums.length+1;
-        }
         
-        return ngtr;
+        int[] res = new int[nums.length - k + 1];
+        int j = 0;
+        for(int i = 0; i < res.length; i++){
+            if(j < i){
+                j = i;
+            }
+            
+            while(nge[j] <= i + k - 1){
+                j = nge[j];
+            }
+            
+            res[i] = nums[j];
+        }
+     
+        return res;
     }
 }
