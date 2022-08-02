7c35b2dc677e7280be33ca781c171302114f9098
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-02 18:20:00 +0530

    Time: 142 ms (68.87%), Space: 96.4 MB (31.79%) - LeetHub

--- 853-car-fleet/853-car-fleet.java
+++ 853-car-fleet/853-car-fleet.java
@@ -1,34 +1,54 @@
-class Solution {
-    class pair implements Comparable<pair>{
-        int pos;
-        int speed;
+// class Solution {
+//     class pair implements Comparable<pair>{
+//         int pos;
+//         int speed;
         
-        pair(int pos,int speed){
-            this.pos = pos;
-            this.speed = speed;
-        }
-        public int compareTo(pair o){
-            return o.pos-this.pos;
-        }
-    }
-    public int carFleet(int target, int[] position, int[] speed) {
-        PriorityQueue<pair> pq = new PriorityQueue<>();
-        for(int i=0;i<position.length;i++){
-            pq.add(new pair(position[i],speed[i]));
-        }
-        double time = 0;
-        int count =0;
-        while(pq.size()!=0){
-            pair p = pq.remove();
-            double ctime = (target-p.pos)/(p.speed*1.0);
+//         pair(int pos,int speed){
+//             this.pos = pos;
+//             this.speed = speed;
+//         }
+//         public int compareTo(pair o){
+//             return o.pos-this.pos;
+//         }
+//     }
+//     public int carFleet(int target, int[] position, int[] speed) {
+//         PriorityQueue<pair> pq = new PriorityQueue<>();
+//         for(int i=0;i<position.length;i++){
+//             pq.add(new pair(position[i],speed[i]));
+//         }
+//         double time = 0;
+//         int count =0;
+//         while(pq.size()!=0){
+//             pair p = pq.remove();
+//             double ctime = (target-p.pos)/(p.speed*1.0);
             
-            if(ctime>time){
-                count++;
+//             if(ctime>time){
+//                 count++;
+//                 time = ctime;
+//             }
+//         }
+        
+        
+//         return count;
+//     }
+// }
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
+        for(int i = 0; i < position.length; i++){
+            map.put(position[i], speed[i]);
+        }
+        
+        double time = 0;
+        int res = 0;
+        for(Map.Entry<Integer, Integer> car: map.entrySet()){
+            double ctime = (target - car.getKey()) * 1.0 / car.getValue();
+            if(ctime > time){
                 time = ctime;
+                res++;
             }
         }
         
-        
-        return count;
+        return res;
     }
 }
