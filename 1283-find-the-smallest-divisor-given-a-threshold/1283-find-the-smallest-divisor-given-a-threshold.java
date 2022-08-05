628001498896e6b00c61d6d47379c11fd4d6b703
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-05 17:47:03 +0530

    Time: 15 ms (85.59%), Space: 55.4 MB (76.56%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,38 +1,61 @@
+// class Solution {
+//     public int smallestDivisor(int[] nums, int threshold) {
+//         int sum = Integer.MIN_VALUE;
+//         for(int ele:nums){
+//             sum=Math.max(ele,sum);
+//         }
+//         int lo = 1;
+//         int hi = sum;
+        
+//         int ans = Integer.MAX_VALUE;
+//         while(lo<=hi){
+//             int mid = lo+(hi-lo)/2;
+//             int res = find(nums,mid);
+            
+//             if(res<=threshold){
+//                 hi = mid-1;
+//                 ans = Math.min(ans,mid);
+//             }
+//             else{
+//                 lo = mid+1;
+//             }
+//         }
+//         return ans;
+    
+//     }
+//     int find(int[] nums,int k){
+//         int h = 0;
+//         for(int ele:nums){
+//             if(ele%k==0){
+//                 h+=ele/k;
+//             }
+//             else{
+//                 h+=ele/k+1;
+//             }
+//         }
+//         return h;
+//     }
+// }
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-        int sum = Integer.MIN_VALUE;
-        for(int ele:nums){
-            sum=Math.max(ele,sum);
-        }
         int lo = 1;
-        int hi = sum;
+        int hi = 1000000;
         
-        int ans = Integer.MAX_VALUE;
-        while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            int res = find(nums,mid);
+        while(lo < hi){
+            int mid = (lo + hi) / 2;
             
-            if(res<=threshold){
-                hi = mid-1;
-                ans = Math.min(ans,mid);
+            int sum = 0;
+            for(int val: nums){
+                sum += (val + mid - 1) / mid;
             }
-            else{
-                lo = mid+1;
-            }
-        }
-        return ans;
-    
-    }
-    int find(int[] nums,int k){
-        int h = 0;
-        for(int ele:nums){
-            if(ele%k==0){
-                h+=ele/k;
-            }
-            else{
-                h+=ele/k+1;
+            
+            if(sum > threshold){
+                lo = mid + 1;
+            } else {
+                hi = mid;
             }
         }
-        return h;
+        
+        return lo;
     }
 }
