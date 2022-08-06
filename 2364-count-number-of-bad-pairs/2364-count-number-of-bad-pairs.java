18073631727819a6b8911c9eda4a1127281e9b43
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-06 22:45:07 +0530

    Time: 107 ms (20.00%), Space: 102 MB (80.00%) - LeetHub

---
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long countBadPairs(int[] nums) {
+        int n = nums.length;
+        
+        long count = 1L*n*(n-1)/2;
+        
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i = 0;i<n;i++){
+            int key = nums[i]-i;
+            hm.put(key,hm.getOrDefault(key,0)+1);
+        }
+        
+        long rej = 0;
+        for(int k:hm.keySet()){
+            if(hm.get(k)>1){
+                int h = hm.get(k);
+                rej += 1L*h*(h-1)/2;
+            }
+        }
+       System.out.print(rej);
+        return count-rej;
+        
+        
+    }
+}
