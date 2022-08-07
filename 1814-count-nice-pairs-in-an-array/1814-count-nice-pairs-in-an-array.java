f04e97af5f6ee967351c44014b966011f500deb3
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-07 15:02:16 +0530

    Time: 53 ms (80.56%), Space: 76.4 MB (51.85%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
@@ -1,52 +1,56 @@
 class Solution {
-//     public int rev(int n){
-//         int ans= 0;
-//         while(n>0){
-//             int rem = n%10;
-//             ans = (ans*10) + rem;
-//             n/=10;    
-//         }
-//         return ans;
-//     }
-//     public int countNicePairs(int[] nums) {
+    public int rev(int n){
+        while(n>0 && n%10==0){
+                n/=10;
+        }
+        int ans= 0;
+        while(n>0){
+            int rem = n%10;
+            ans = (ans*10) + rem;
+            n/=10;    
+        }
         
-//         int n = nums.length;
+        return ans;
+    }
+    public int countNicePairs(int[] nums) {
+        
+        int n = nums.length;
         
         
         
-//         HashMap<Integer,Integer> hm = new HashMap<>();
-//         for(int i = 0;i<n;i++){
-//             int key = nums[i]-rev(nums[i]);
-//             hm.put(key,hm.getOrDefault(key,0)+1);
-//         }
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i = 0;i<n;i++){
+            int key = nums[i]-rev(nums[i]);
+            hm.put(key,hm.getOrDefault(key,0)+1);
+        }
         
-//         int rej = 0;
-//         for(int k:hm.keySet()){
-//             if(hm.get(k)>1){
-//                 int h = hm.get(k);
-//                 rej += h*(h-1)/2;
-//             }
-//         }
+        long rej = 0;
+        for(int k:hm.keySet()){
+            if(hm.get(k)>1){
+                long h = hm.get(k);
+                rej=((rej)%1000000007+ h*(h-1)/2)%1000000007;
+            }
+        }
  
-//         return rej%1000000007;
+        return (int)rej%1000000007;
+    }
+//      public int countNicePairs(int[] A) {
+//         int res = 0, mod = (int)1e9 + 7;
+//         Map<Integer, Integer> count = new HashMap<>();;
+//         for (int a : A) {
+//             int b = rev(a), v = count.getOrDefault(a - b, 0);
+//             count.put(a - b, v + 1);
+//             res = (res + v) % mod;
+//         }
+//         return res;
 //     }
-     public int countNicePairs(int[] A) {
-        int res = 0, mod = (int)1e9 + 7;
-        Map<Integer, Integer> count = new HashMap<>();;
-        for (int a : A) {
-            int b = rev(a), v = count.getOrDefault(a - b, 0);
-            count.put(a - b, v + 1);
-            res = (res + v) % mod;
-        }
-        return res;
-    }
 
-    public int rev(int a) {
-        int b = 0;
-        while (a > 0) {
-            b = b * 10 + (a % 10);
-            a /= 10;
-        }
-        return b;
-    }
+//     public int rev(int a) {
+//         int b = 0;
+//         while (a > 0) {
+//             b = b * 10 + (a % 10);
+//             a /= 10;
+//         }
+//         return b;
+//     }
 }
