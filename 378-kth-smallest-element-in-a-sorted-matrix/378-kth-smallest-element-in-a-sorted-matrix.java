dcee116740f3d7561971e2e6922994dbb59ef9ec
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-11 10:44:43 +0530

    Time: 2 ms (74.98%), Space: 57.6 MB (19.53%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,42 +1,70 @@
-// class Solution {
-//     public int kthSmallest(int[][] matrix, int k) {
-        
-//     }
-// }
-
 class Solution {
-    class pair implements Comparable<pair>{
-        int r;
-        int c;
-        int val;
-        pair(int r,int c,int val){
-            this.r = r;
-            this.c = c;
-            this.val = val;
-        }
-        public int compareTo(pair o){
-            return this.val-o.val;
-        }
-        
-        
-    }
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<pair> pq = new PriorityQueue<>();
-        int n = matrix.length;
-        for(int i=0;i<matrix[0].length;i++){
-            pq.add(new pair(0,i,matrix[0][i]));
-        }
+        int m = matrix.length;
+        int n = matrix[0].length;
         
-        int f = 0;
-        int val = 0;
-        while(f<k){
-            pair rem = pq.remove();
-            f++;
-            val = rem.val;
-            if(rem.r<n-1){
-                pq.add(new pair(rem.r+1,rem.c,matrix[rem.r+1][rem.c]));
+        int lo = matrix[0][0];
+        int hi =matrix[m-1][n-1];
+        
+        while(lo<hi){
+            int mid = lo+(hi-lo)/2;
+            
+            int count = 0;
+            int j = n-1;
+            for(int i =0;i<m;i++){
+                while(j>=0 && matrix[i][j]>mid){
+                    j--;
+                }
+                if(j==-1){
+                    break;
+                }
+                count+=j+1;
+            }
+            
+            if(count<k){
+                lo = mid+1;
+            }
+            else{
+                hi = mid;
             }
         }
-        return val;
+        return lo;
     }
 }
+
+// class Solution {
+//     class pair implements Comparable<pair>{
+//         int r;
+//         int c;
+//         int val;
+//         pair(int r,int c,int val){
+//             this.r = r;
+//             this.c = c;
+//             this.val = val;
+//         }
+//         public int compareTo(pair o){
+//             return this.val-o.val;
+//         }
+        
+        
+//     }
+//     public int kthSmallest(int[][] matrix, int k) {
+//         PriorityQueue<pair> pq = new PriorityQueue<>();
+//         int n = matrix.length;
+//         for(int i=0;i<matrix[0].length;i++){
+//             pq.add(new pair(0,i,matrix[0][i]));
+//         }
+        
+//         int f = 0;
+//         int val = 0;
+//         while(f<k){
+//             pair rem = pq.remove();
+//             f++;
+//             val = rem.val;
+//             if(rem.r<n-1){
+//                 pq.add(new pair(rem.r+1,rem.c,matrix[rem.r+1][rem.c]));
+//             }
+//         }
+//         return val;
+//     }
+// }
