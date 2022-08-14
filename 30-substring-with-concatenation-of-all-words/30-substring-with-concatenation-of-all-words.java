882219876978c9da553e29d4d8451324a4c16856
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-14 21:37:45 +0530

    Time: 322 ms (31.82%), Space: 72.7 MB (46.92%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -1,45 +1,96 @@
-// class Solution {
-//     public List<Integer> findSubstring(String s, String[] words) {
-//         HashMap<String,Integer> freq = new HashMap<>();
-//         int n = words[0].length();
-        
-//         for(String w:words){
-//             freq.put(freq.getOrDefault(w,0)+1);
-//         }
-        
-//         HashMap<String,Integer> copy = freq;
-//         int i = 0;
-//         while(i<(s.length()-(n*words.length)))
-        
-//     }
-// }
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        final Map<String, Integer> counts = new HashMap<>();
-        for (final String word : words) {
-            counts.put(word, counts.getOrDefault(word, 0) + 1);
+        HashMap<String,Integer> copy = new HashMap<>();
+        int n = words[0].length();
+        int m = words.length;
+        if(s.length()<m*n){
+            return new ArrayList<>();
         }
-        final List<Integer> indexes = new ArrayList<>();
-        final int n = s.length(), num = words.length, len = words[0].length();
-        for (int i = 0; i < n - num * len + 1; i++) {
-            final Map<String, Integer> seen = new HashMap<>();
-            int j = 0;
-            while (j < num) {
-                final String word = s.substring(i + j * len, i + (j + 1) * len);
-                if (counts.containsKey(word)) {
-                    seen.put(word, seen.getOrDefault(word, 0) + 1);
-                    if (seen.get(word) > counts.getOrDefault(word, 0)) {
-                        break;
+        
+        for(String w:words){
+            copy.put(w,copy.getOrDefault(w,0)+1);
+        }
+        List<Integer> ans = new ArrayList<>();
+        
+        
+        
+        int i = 0;
+        while(i<=(s.length()-(n*words.length))){
+           String key = s.substring(i,i+n);
+            
+            if(copy.containsKey(key)){
+                int j = i+n;
+                
+                while(copy.containsKey(key)){
+                    
+                    copy.put(key,copy.get(key)-1);
+                    if(copy.get(key)==0){
+                        copy.remove(key);
                     }
-                } else {
-                    break;
+                    key = s.substring(j,Math.min(s.length(),(j+n)));
+                    
+                    j = j+n;
                 }
-                j++;
+                
+                
+                if(copy.size()==0){
+                    ans.add(i);
+                    i++;
+                    
+                    for(String w:words){
+                        copy.put(w,copy.getOrDefault(w,0)+1);
+                    }
+                    
+                }
+                else{
+                    
+                    
+                    copy = new HashMap<>();
+                    for(String w:words){
+                        copy.put(w,copy.getOrDefault(w,0)+1);
+                    }
+                    i++;
+                }
+                
+                
             }
-            if (j == num) {
-                indexes.add(i);
+            
+            
+            else{
+                i++;
             }
         }
-        return indexes;
+        return ans;
+        
     }
 }
+// class Solution {
+//     public List<Integer> findSubstring(String s, String[] words) {
+//         final Map<String, Integer> counts = new HashMap<>();
+//         for (final String word : words) {
+//             counts.put(word, counts.getOrDefault(word, 0) + 1);
+//         }
+//         final List<Integer> indexes = new ArrayList<>();
+//         final int n = s.length(), num = words.length, len = words[0].length();
+//         for (int i = 0; i < n - num * len + 1; i++) {
+//             final Map<String, Integer> seen = new HashMap<>();
+//             int j = 0;
+//             while (j < num) {
+//                 final String word = s.substring(i + j * len, i + (j + 1) * len);
+//                 if (counts.containsKey(word)) {
+//                     seen.put(word, seen.getOrDefault(word, 0) + 1);
+//                     if (seen.get(word) > counts.getOrDefault(word, 0)) {
+//                         break;
+//                     }
+//                 } else {
+//                     break;
+//                 }
+//                 j++;
+//             }
+//             if (j == num) {
+//                 indexes.add(i);
+//             }
+//         }
+//         return indexes;
+//     }
+// }
