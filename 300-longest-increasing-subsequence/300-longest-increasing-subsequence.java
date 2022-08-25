7f4c78ec551a3d9accfd2f1991f51f693ac8f9ad
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-25 23:48:08 +0530

    Time: 3 ms (98.67%), Space: 43.8 MB (82.24%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -15,25 +15,52 @@
 //         return ans;
 //     }
 // }
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int[] lis = new int[nums.length];
+//         lis[0] = 1;
+        
+//         for(int i = 1; i < lis.length; i++){
+//             int max = 0;
+//             for(int j = 0; j < i; j++){
+//                 if(nums[j] < nums[i]){
+//                     max = Math.max(max, lis[j]);
+//                 }
+//             }
+            
+//             lis[i] = max + 1;
+//         }
+
+//         int len = 0;
+//         for(int val: lis){
+//             len = Math.max(val, len);
+//         }
+        
+//         return len;
+//     }
+// }
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] lis = new int[nums.length];
-        lis[0] = 1;
+        int n = nums.length;
+        int len = 1;
+        int[] tail = new int[n];
+        tail[0] = nums[0];
         
-        for(int i = 1; i < lis.length; i++){
-            int max = 0;
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i]){
-                    max = Math.max(max, lis[j]);
-                }
+        for(int i = 0;i<n;i++){
+            if(nums[i]>tail[len-1]){
+                len++;
+                tail[len-1] = nums[i];
             }
-            
-            lis[i] = max + 1;
-        }
-
-        int len = 0;
-        for(int val: lis){
-            len = Math.max(val, len);
+            else{
+                int idx = Arrays.binarySearch(tail,0,len-1,nums[i]);
+                
+                if(idx<0){
+                    idx = -idx;
+                    idx = idx-1;
+                }
+                tail[idx] = nums[i];
+            }
         }
         
         return len;
