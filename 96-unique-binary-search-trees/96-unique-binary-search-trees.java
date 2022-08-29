a9593525c4f387eec628a0773bb30a4ad8e0095c
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-29 14:40:16 +0530

    Time: 0 ms (100.00%), Space: 42 MB (7.76%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,20 +1,17 @@
 class Solution {
     public int numTrees(int n) {
-        if(n==0 || n==1){
-            return 1;
-        } 
-        
-        int[] G = new int[n+1];
-        G[0]=1;
-        G[1]=1;
-        
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                G[i] += G[j-1]*G[i-j];
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i =2;i<=n;i++){
+            int left = 0;
+            int right = i-1;
+            while(left <= i-1){
+                dp[i] += dp[left] * dp[right];
+                left++;
+                right--;
             }
         }
-            
-            
-        return G[n];
+        return dp[n];
     }
 }
