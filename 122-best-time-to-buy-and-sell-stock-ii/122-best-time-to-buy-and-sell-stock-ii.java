095a3b793fa5f345ff92b2c0518e5f4d8f6b74dc
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-31 14:27:35 +0530

    Time: 2 ms (41.80%), Space: 44.2 MB (53.69%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,19 +2,19 @@ class Solution {
     public int maxProfit(int[] stocks) {
         int b = 0,s = 0;
         int ans = 0;
-        int i = 0;
-        while(i < stocks.length-1){
-            while(i < stocks.length-1 && stocks[i]<=stocks[i+1] ){
+        // int i = 0;
+        while(s < stocks.length-1){
+            while(s < stocks.length-1 && stocks[s]<=stocks[s+1] ){
                 s++;
-                i++;
+                // i++;
             }
             ans += stocks[s]-stocks[b];
             b= s;
-            while(i < stocks.length-1 && stocks[i]>stocks[i+1]){
+            while(s < stocks.length-1 && stocks[s]>stocks[s+1]){
                 // b=s;
                 b++;
                 s++;
-                i++;
+                // i++;
             }
             
         }
