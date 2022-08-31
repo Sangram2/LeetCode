1b58ea7c60c7545c45f6670b2b6e93f358a3c2b3
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-08-31 21:53:06 +0530

    Time: 2 ms (55.06%), Space: 42.6 MB (33.66%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,39 +1,19 @@
-// class Solution {
-//     public int maxProfit(int[] prices) {
-//         int b = -prices[0];
-//         int s = 0;
-//         int c = 0;
-//         for(int i=1;i<prices.length;i++){
-//             int val = prices[i];
-            
-//             int nb = Math.max(c-val,b);
-//             int ns = Math.max(b+val,s);
-//             int nc = Math.max(c,s);
-            
-//             nb = b;
-//             ns = s;
-//             nc = c;
-//         }
-//         return s;
-//     }
-// }
 class Solution {
     public int maxProfit(int[] prices) {
         int b = -prices[0];
         int s = 0;
         int c = 0;
-        
-        for(int i = 1; i < prices.length; i++){
+        for(int i=1;i<prices.length;i++){
             int val = prices[i];
-            int nbsp = Math.max(c - val, b);
-            int nssp = Math.max(b + val, s);
-            int ncsp = Math.max(s, c);
             
-            b = nbsp;
-            s = nssp;
-            c = ncsp;
+            int nb = Math.max(c-val,b);
+            int ns = Math.max(b+val,s);
+            int nc = Math.max(c,s);
+            
+            b = nb;
+            s = ns;
+            c = nc;
         }
-        
         return s;
     }
 }
