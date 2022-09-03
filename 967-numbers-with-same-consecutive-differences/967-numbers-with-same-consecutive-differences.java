81c6f05ac3bb1e053c057430187455f004cec61a
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-09-03 19:47:39 +0530

    Time: 3 ms (80.94%), Space: 43.6 MB (20.31%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,21 +1,26 @@
 class Solution {
-    public int[] numsSameConsecDiff(int N, int K) {
-        List<Integer> cur = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        for (int i = 2; i <= N; ++i) {
-            List<Integer> cur2 = new ArrayList<>();
-            for (int x : cur) {
-                int y = x % 10;
-                if (y + K < 10)
-                    cur2.add(x * 10 + y + K);
-                if (K > 0 && y - K >= 0)
-                    cur2.add(x * 10 + y - K);
+    public int[] numsSameConsecDiff(int N, int k) {
+        List<Integer> cur = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        for(int i = 2;i<=N;i++){
+            List<Integer> curr = new ArrayList<>();
+            for(int x:cur){
+                int y = x%10;
+                if(y+k<10){
+                    curr.add(10*x+ (y+k));
+                }
+                
+                if(k>0 && y-k>=0){
+                    curr.add(10*x+ (y-k));
+                }
             }
-            cur = cur2;
+            cur = curr;
         }
-        int[] arr = new int[cur.size()];
-        for(int i = 0;i<arr.length;i++){
-            arr[i] = cur.get(i);
+        
+        int[] ans = new int[cur.size()];
+        for(int i = 0;i<ans.length;i++){
+            ans[i] = cur.get(i);
         }
-        return arr;
+        
+        return ans;
     }
 }
