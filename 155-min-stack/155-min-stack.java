9f41db09cd9e36ff184722fc0d2a8293eaba878a
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-09-06 20:40:14 +0530

    Time: 10 ms (28.79%), Space: 48.5 MB (54.92%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,39 +1,45 @@
 class MinStack {
-    Stack<Integer> data;
-    Stack<Integer> min;
+    Stack<Long> data;
+    Long min;
+
     public MinStack() {
         data = new Stack<>();
-        min = new Stack<>();
+        min = Long.MAX_VALUE;
     }
     
     public void push(int val) {
+        long val1 = val;
+        
         if(data.size() == 0){
-            data.push(val);
-            min.push(val);
+            data.push(val1);
+            min = val1;
+        } else if(val1 >= min){
+            data.push(val1);
         } else {
-            data.push(val);
-            if(val <= min.peek()){
-                min.push(val);
-            }
+            data.push(val1 + val1 - min);
+            min = val1;
         }
     }
     
     public void pop() {
-        if(data.peek().equals(min.peek())){
+        if(data.peek() >= min){
             data.pop();
-            min.pop();
-        }
-        else{
+        } else {
+            min = 2 * min - data.peek();
             data.pop();
         }
     }
     
     public int top() {
-        return data.peek();
+        if(data.peek() >= min){
+            return data.peek().intValue();
+        } else {
+            return min.intValue();
+        }
     }
     
     public int getMin() {
-        return min.peek();
+        return min.intValue();
     }
 }
 
@@ -45,3 +51,52 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+
+// class MinStack {
+//     Stack<Integer> data;
+//     Stack<Integer> min;
+//     public MinStack() {
+//         data = new Stack<>();
+//         min = new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         if(data.size() == 0){
+//             data.push(val);
+//             min.push(val);
+//         } else {
+//             data.push(val);
+//             if(val <= min.peek()){
+//                 min.push(val);
+//             }
+//         }
+//     }
+    
+//     public void pop() {
+//         if(data.peek().equals(min.peek())){
+//             data.pop();
+//             min.pop();
+//         }
+//         else{
+//             data.pop();
+//         }
+//     }
+    
+//     public int top() {
+//         return data.peek();
+//     }
+    
+//     public int getMin() {
+//         return min.peek();
+//     }
+// }
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack obj = new MinStack();
+//  * obj.push(val);
+//  * obj.pop();
+//  * int param_3 = obj.top();
+//  * int param_4 = obj.getMin();
+//  */
