760254d6e55f9735c945f588ce37a072ea3c0658
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-09-11 15:28:34 +0530

    Time: 103 ms (25.00%), Space: 72.2 MB (12.67%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,17 +1,17 @@
 class Solution {
-//     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-//         int[][] es = new int[n][2];
-//         for(int i = 0;i<n;i++){
-//             int[] ess = new int[]{efficiency[i],speed[i]};
-//             es[i] = ess;
-//         }
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        int[][] es = new int[n][2];
+        for(int i = 0;i<n;i++){
+            int[] ess = new int[]{efficiency[i],speed[i]};
+            es[i] = ess;
+        }
         
-//         Arrays.sort(es,(a,b)->{
-//             return b[0]-a[0];
-//         });
-//         PriorityQueue<Integer> pq = new PriorityQueue<>(k);
-//         long max = 0,mod = (long)1e9+7;
-//         long sum = 0;
+        Arrays.sort(es,(a,b)->{
+            return b[0]-a[0];
+        });
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+        long res = 0,mod = (long)1e9+7;
+        long sumS = 0;
 //         for(int[] arr:es){
 //             sum=(sum+arr[1])%mod;
 //             pq.add(arr[1]);
@@ -21,21 +21,28 @@ class Solution {
 //             }
 //             max = (long)Math.max((arr[0]*sum)%mod,max);
 //         }
-//         return (int)(max%mod);
-//     }
-    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int[][] ess = new int[n][2];
-        for (int i = 0; i < n; ++i)
-            ess[i] = new int[] {efficiency[i], speed[i]};
-        Arrays.sort(ess, (a, b) -> b[0] - a[0]);
-        PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);
-        long res = 0, sumS = 0;
-        for (int[] es : ess) {
-            pq.add(es[1]);
-            sumS = (sumS + es[1]);
+        for (int[] arr : es) {
+            pq.add(arr[1]);
+            sumS = (sumS + arr[1]);
             if (pq.size() > k) sumS -= pq.poll();
-            res = Math.max(res, (sumS * es[0]));
+            res = Math.max(res, (sumS * arr[0]));
         }
-        return (int) (res % (long)(1e9 + 7));
+        
+        return (int)(res%mod);
     }
+    // public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+    //     int[][] ess = new int[n][2];
+    //     for (int i = 0; i < n; ++i)
+    //         ess[i] = new int[] {efficiency[i], speed[i]};
+    //     Arrays.sort(ess, (a, b) -> b[0] - a[0]);
+    //     PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);
+    //     long res = 0, sumS = 0;
+    //     for (int[] es : ess) {
+    //         pq.add(es[1]);
+    //         sumS = (sumS + es[1]);
+    //         if (pq.size() > k) sumS -= pq.poll();
+    //         res = Math.max(res, (sumS * es[0]));
+    //     }
+    //     return (int) (res % (long)(1e9 + 7));
+    // }
 }
