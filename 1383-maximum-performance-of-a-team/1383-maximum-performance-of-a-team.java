7e6ec69849f80489353be745f9ee00f827bcfc3c
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-09-11 15:29:35 +0530

    Time: 114 ms (17.33%), Space: 67.8 MB (77.33%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -12,15 +12,7 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>(k);
         long res = 0,mod = (long)1e9+7;
         long sumS = 0;
-//         for(int[] arr:es){
-//             sum=(sum+arr[1])%mod;
-//             pq.add(arr[1]);
-//             if(pq.size()>k){
-//                 sum =(sum-pq.remove())%mod;
-                
-//             }
-//             max = (long)Math.max((arr[0]*sum)%mod,max);
-//         }
+      
         for (int[] arr : es) {
             pq.add(arr[1]);
             sumS = (sumS + arr[1]);
@@ -30,19 +22,5 @@ class Solution {
         
         return (int)(res%mod);
     }
-    // public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-    //     int[][] ess = new int[n][2];
-    //     for (int i = 0; i < n; ++i)
-    //         ess[i] = new int[] {efficiency[i], speed[i]};
-    //     Arrays.sort(ess, (a, b) -> b[0] - a[0]);
-    //     PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);
-    //     long res = 0, sumS = 0;
-    //     for (int[] es : ess) {
-    //         pq.add(es[1]);
-    //         sumS = (sumS + es[1]);
-    //         if (pq.size() > k) sumS -= pq.poll();
-    //         res = Math.max(res, (sumS * es[0]));
-    //     }
-    //     return (int) (res % (long)(1e9 + 7));
-    // }
+   
 }
