5f28338f070db2bd8fd068bb5ae1d292184a5739
Sangram Keshari Ghosh <79435858+Sangram2@users.noreply.github.com>
2022-09-15 22:11:58 +0530

    Time: 75 ms (64.71%), Space: 43.7 MB (53.98%) - LeetHub

--- 313-super-ugly-number/313-super-ugly-number.java
+++ 313-super-ugly-number/313-super-ugly-number.java
@@ -1,47 +1,79 @@
-class Solution {
-//     public int nthSuperUglyNumber(int n, int[] primes) {
-//         PriorityQueue<Integer> pq = new PriorityQueue<>();
-//         HashSet<Integer> hs = new HashSet<>();
-//         pq.add(1);
-//         int st = 1;
-//         while(n-->0){
-//             st = pq.remove();
+// class Solution {
+// //     public int nthSuperUglyNumber(int n, int[] primes) {
+// //         PriorityQueue<Integer> pq = new PriorityQueue<>();
+// //         HashSet<Integer> hs = new HashSet<>();
+// //         pq.add(1);
+// //         int st = 1;
+// //         while(n-->0){
+// //             st = pq.remove();
             
-//             for(int p:primes){
-//                 if(hs.contains(st*p)){
-//                     continue;
+// //             for(int p:primes){
+// //                 if(hs.contains(st*p)){
+// //                     continue;
+// //                 }
+// //                 pq.add(st*p);
+// //                 hs.add(st*p);
+// //             }
+// //         }
+// //         return st;
+// //     }
+//     public int nthSuperUglyNumber(int n, int[] primes) {
+//         int[] pos = new int[primes.length];
+//         Arrays.fill(pos,1);
+//         int[] dp =new int[n+1];
+//         dp[1] = 1;
+//         for(int j = 2;j <= n; j++){
+//             long min = Long.MAX_VALUE;
+//             for(int i = 0;i<pos.length;i++){
+//                 int p = pos[i];
+//                 int prim = primes[i];
+//                 int val = dp[p];
+//                 min = Math.min((long)min,(long)val*prim);
+//             }
+//             dp[j] = (int)min;
+//             for(int i = 0;i<pos.length;i++){
+//                 int p = pos[i];
+//                 int prim = primes[i];
+//                 int val = dp[p];
+//                 if(min == val*prim){
+//                     pos[i] = pos[i]+1;
 //                 }
-//                 pq.add(st*p);
-//                 hs.add(st*p);
 //             }
 //         }
-//         return st;
+        
+//         return dp[n];
+        
 //     }
+// }
+class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        int[] pos = new int[primes.length];
-        Arrays.fill(pos,1);
-        int[] dp =new int[n+1];
+        long[] dp = new long[n + 1];
         dp[1] = 1;
-        for(int j = 2;j <= n; j++){
-            long min = Long.MAX_VALUE;
-            for(int i = 0;i<pos.length;i++){
-                int p = pos[i];
-                int prim = primes[i];
-                int val = dp[p];
-                min = Math.min((long)min,(long)val*prim);
+        
+        int[] idxs = new int[primes.length];
+        Arrays.fill(idxs, 1);
+        
+        for(int i = 2; i < dp.length; i++){
+            long ans = Long.MAX_VALUE;
+            for(int j = 0; j < primes.length; j++){
+                int prime = primes[j];
+                int idx = idxs[j];
+                
+                ans = Math.min(ans, dp[idx] * prime);
             }
-            dp[j] = (int)min;
-            for(int i = 0;i<pos.length;i++){
-                int p = pos[i];
-                int prim = primes[i];
-                int val = dp[p];
-                if(min == val*prim){
-                    pos[i] = pos[i]+1;
+            
+            for(int j = 0; j < primes.length; j++){
+                int prime = primes[j];
+                int idx = idxs[j];
+                
+                if(ans == dp[idx] * prime){
+                    idxs[j] = idx + 1;
                 }
             }
+            
+            dp[i] = ans;
         }
         
-        return dp[n];
-        
+        return (int)dp[n];
     }
 }
